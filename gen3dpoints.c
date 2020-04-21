eed0e5423bf58295c5e739df5e85094427781a99
kylo887 <57532157+kylo887@users.noreply.github.com>
2020-04-22 01:21:04 +0200

    second random also didnt work. will work with rand and srand

--- gen3dpoints.c
+++ gen3dpoints.c
@@ -1,4 +1,4 @@
-#include <random>
+/*#include <random>
 
 std::mt19937 eng; // object which produces random bits
 
@@ -15,3 +15,39 @@ for(int i = 0; i < 6; i++) {
 
   std::cout << "x:" << x << " y:" << y << "\n\n";
 }
+*/
+
+/*#include <random>
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+    const int N = 10;
+    std::vector<std::string> square(N, std::string(N, '*'));
+    
+    for (auto &&row : square) {
+        std::cout << row << '\n';
+    }
+    
+    std::mt19937 eng;
+    eng.seed(std::random_device{}());
+
+    std::uniform_int_distribution<> dist(0, N-1);
+    
+    for(int i = 0; i < 6; i++) {
+      int x = dist(eng);
+      int y = dist(eng);
+      
+      std::cout << "x:" << x << " y:" << y << "\n\n";
+      
+      square[y][x] = ' ';
+    }
+    
+    for (auto &&row : square) {
+        std::cout << row << '\n';
+    }
+
+
+}
+*/
